4fd1684bbb487a0fcd49aac6c799610a95253372
Mohammed ROUAMCHI <mohamed.rouamchi96@gmail.com>
2024-06-14 17:41:20 +0100

    EDIT

--- server/app.js
+++ server/app.js
@@ -9,6 +9,55 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const productsRouter = require('./routes/products');
 const app = express();
+////////////////////////////////////////////
+const debug = require('debug')('server:server');
+const http = require('http');
+const port = normalizePort(process.env.PORT || '4000');
+app.set('port', port);
+const server = http.createServer(app);
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+function normalizePort(val) {
+  const port = parseInt(val, 10);
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+}
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+function onListening() {
+  const addr = server.address();
+  const bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
+
+////////////////////////////////////////////
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
