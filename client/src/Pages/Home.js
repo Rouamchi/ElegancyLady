5255edad39fe1fcac9b4bed6f1321d996dfb4a35
Mohammed ROUAMCHI <mohamed.rouamchi96@gmail.com>
2024-06-20 16:46:43 +0100

    edit

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
 
   useEffect(() => {
     const fetchProducts = async () => {
-      const { data } = await axios.get('https://elegancyladyserver.onrender.com/products')
+      const { data } = await axios.get('https://elegancyladyserver.onrender.com/products/uploads')
       setProducts(data)
     }
     fetchProducts()
