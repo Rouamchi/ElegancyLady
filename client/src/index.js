5f8956fcb2c0645e700ed69894cb9f3a0a4a828f
Mohammed ROUAMCHI <mohamed.rouamchi96@gmail.com>
2024-06-25 14:13:15 +0100

    EDIT

--- client/src/index.js
+++ client/src/index.js
@@ -64,7 +64,7 @@ const router = createBrowserRouter([
     element: <Cart />,
   },
   {
-    path: "/Favorites/:id?",
+    path: "https://elegancyladydiastore.onrender.com/Favorites/:id?",
     element: <Favorites />,
   },
   {
