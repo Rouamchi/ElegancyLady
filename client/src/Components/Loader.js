5fc0d5adfb70001d2fee6c9c13681bd22412a610
Mohammed ROUAMCHI <mohamed.rouamchi96@gmail.com>
2024-07-02 15:35:45 +0100

    EDIT

--- client/src/Components/Loader.js
+++ client/src/Components/Loader.js
@@ -2,33 +2,105 @@ import React from 'react'
 const Loader = () => {
   return (
     <>
-      <div className="card rounded-[12px] m-2 md:m-4 text-xl ml-4 shadow-2xl p-2 md:p-5 animate-pulse">
+      <div className="rounded-[12px] text-xl ml-3 md:ml-6 mb-0 md:mb-2 shadow-xl p-2 md:p-3 animate-pulse">
         <a href="/">
-          <div className="w-full h-32 md:h-64 bg-gray-200 rounded mb-3" />
+          <div className="w-full h-32 md:h-64 bg-gray-200 rounded mb-3">
+            <div className="flex items-center justify-center w-full h-full">
+              <div className="flex justify-center items-center space-x-1 text-sm text-gray-700">
+                <svg
+                  fill="none"
+                  className="w-8 h-8 animate-spin"
+                  viewBox="0 0 32 32"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    clipRule="evenodd"
+                    d="M15.165 8.53a.5.5 0 01-.404.58A7 7 0 1023 16a.5.5 0 011 0 8 8 0 11-9.416-7.874.5.5 0 01.58.404z"
+                    fill="currentColor"
+                    fillRule="evenodd"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
           <div className="h-4 bg-gray-200 rounded w-3/4 mb-2" />
           <div className="h-1 bg-gray-200 rounded w-full mb-2" />
           <div className="h-4 bg-gray-200 rounded w-1/4" />
         </a>
       </div>
-      <div className="card rounded-[12px] m-2 md:m-4 text-xl mr-4 shadow-2xl p-2 md:p-5 animate-pulse">
+      <div className="rounded-[12px] text-xl ml-0 md:ml-4 mr-3 md:mr-2 shadow-xl mb-0 md:mb-2 p-2 md:p-3 animate-pulse">
         <a href="/">
-          <div className="w-full h-32 md:h-64 bg-gray-200 rounded mb-3" />
+          <div className="w-full h-32 md:h-64 bg-gray-200 rounded mb-3">
+          <div className="flex items-center justify-center w-full h-full">
+              <div className="flex justify-center items-center space-x-1 text-sm text-gray-700">
+                <svg
+                  fill="none"
+                  className="w-8 h-8 animate-spin"
+                  viewBox="0 0 32 32"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    clipRule="evenodd"
+                    d="M15.165 8.53a.5.5 0 01-.404.58A7 7 0 1023 16a.5.5 0 011 0 8 8 0 11-9.416-7.874.5.5 0 01.58.404z"
+                    fill="currentColor"
+                    fillRule="evenodd"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
           <div className="h-4 bg-gray-200 rounded w-3/4 mb-2" />
           <div className="h-1 bg-gray-200 rounded w-full mb-2" />
           <div className="h-4 bg-gray-200 rounded w-1/4" />
         </a>
       </div>
-      <div className="card rounded-[12px] m-2 md:m-4 text-xl ml-4 shadow-2xl p-2 md:p-5 animate-pulse">
+      <div className="rounded-[12px] text-xl ml-3 md:ml-2 mr-0 md:mr-4 shadow-xl mb-2 p-2 md:p-3 animate-pulse">
         <a href="/">
-          <div className="w-full h-32 md:h-64 bg-gray-200 rounded mb-3" />
+          <div className="w-full h-32 md:h-64 bg-gray-200 rounded mb-3">
+          <div className="flex items-center justify-center w-full h-full">
+              <div className="flex justify-center items-center space-x-1 text-sm text-gray-700">
+                <svg
+                  fill="none"
+                  className="w-8 h-8 animate-spin"
+                  viewBox="0 0 32 32"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    clipRule="evenodd"
+                    d="M15.165 8.53a.5.5 0 01-.404.58A7 7 0 1023 16a.5.5 0 011 0 8 8 0 11-9.416-7.874.5.5 0 01.58.404z"
+                    fill="currentColor"
+                    fillRule="evenodd"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
           <div className="h-4 bg-gray-200 rounded w-3/4 mb-2" />
           <div className="h-1 bg-gray-200 rounded w-full mb-2" />
           <div className="h-4 bg-gray-200 rounded w-1/4" />
         </a>
       </div>
-      <div className="card rounded-[12px] m-2 md:m-4 text-xl mr-4 shadow-2xl p-2 md:p-5 animate-pulse">
+      <div className="rounded-[12px] text-xl mr-3 md:mr-6 shadow-xl mb-2 p-2 md:p-3 animate-pulse">
         <a href="/">
-          <div className="w-full h-32 md:h-64 bg-gray-200 rounded mb-3" />
+          <div className="w-full h-32 md:h-64 bg-gray-200 rounded mb-3">
+          <div className="flex items-center justify-center w-full h-full">
+              <div className="flex justify-center items-center space-x-1 text-sm text-gray-700">
+                <svg
+                  fill="none"
+                  className="w-8 h-8 animate-spin"
+                  viewBox="0 0 32 32"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    clipRule="evenodd"
+                    d="M15.165 8.53a.5.5 0 01-.404.58A7 7 0 1023 16a.5.5 0 011 0 8 8 0 11-9.416-7.874.5.5 0 01.58.404z"
+                    fill="currentColor"
+                    fillRule="evenodd"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
           <div className="h-4 bg-gray-200 rounded w-3/4 mb-2" />
           <div className="h-1 bg-gray-200 rounded w-full mb-2" />
           <div className="h-4 bg-gray-200 rounded w-1/4" />
