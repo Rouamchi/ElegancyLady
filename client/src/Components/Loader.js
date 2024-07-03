c974e0e931fafcbebff03344432c5c73298cf6d2
Mohammed ROUAMCHI <mohamed.rouamchi96@gmail.com>
2024-07-03 02:45:43 +0100

    edit

--- client/src/Components/Loader.js
+++ client/src/Components/Loader.js
@@ -2,54 +2,54 @@ import React from 'react'
 const Loader = () => {
   return (
     <>
-        <div className="rounded-[12px] text-xl mb-0 md:mb-2 p-0 animate-pulse">
-          <a href="/">
-            <div className="w-full h-48 md:h-80 bg-gray-200 rounded md:rounded-2xl mb-3">
-              {/* <div className="flex items-center justify-center w-full h-full">
+      <div className="rounded-[12px] text-xl mb-0 md:mb-2 p-0 animate-pulse">
+        <a href="/">
+          <div className="w-full h-48 md:h-80 bg-gray-200 rounded md:rounded-2xl mb-3">
+            {/* <div className="flex items-center justify-center w-full h-full">
                 <div className="flex justify-center items-center space-x-1 text-sm text-gray-500">
                   <div className="w-2 h-2 bg-gray-400 rounded-full" />
                   <div className="w-2 h-2 bg-gray-400 rounded-full" />
                   <div className="w-2 h-2 bg-gray-400 rounded-full" />
                 </div>
               </div> */}
+          </div>
+          <div className="h-4 bg-gray-200 rounded w-2/4 mb-2 ml-1" />
+          <div className="h-1 bg-gray-200 rounded w-2/4 mb-2 ml-1" />
+          <div className="h-8 bg-gray-200 rounded w-4/4 ml-1 mr-1" />
+        </a>
+      </div>
+      <div className="rounded-[12px] text-xl mb-0 md:mb-2 p-0 animate-pulse">
+        <a href="/">
+          <div className="w-full h-48 md:h-80 bg-gray-200 rounded md:rounded-2xl mb-3">
+            <div className="flex items-center justify-center w-full h-full">
             </div>
-            <div className="h-4 bg-gray-200 rounded w-2/4 mb-2 ml-1" />
-            <div className="h-1 bg-gray-200 rounded w-2/4 mb-2 ml-1" />
-            <div className="h-8 bg-gray-200 rounded w-4/4 ml-1 mr-1" />
-          </a>
-        </div>
-        <div className="rounded-[12px] text-xl mb-0 md:mb-2 p-0 animate-pulse">
-          <a href="/">
-            <div className="w-full h-48 md:h-80 bg-gray-200 rounded md:rounded-2xl mb-3">
-              <div className="flex items-center justify-center w-full h-full">
-              </div>
-            </div>
-            <div className="h-4 bg-gray-200 rounded w-2/4 mb-2 ml-1" />
-            <div className="h-1 bg-gray-200 rounded w-2/4 mb-2 ml-1" />
-            <div className="h-8 bg-gray-200 rounded w-4/4 ml-1 mr-1" />
-          </a>
-        </div>
-        <div className="rounded-[12px] text-xl mb-2 p-0 animate-pulse">
-          <a href="/">
-            <div className="w-full h-48 md:h-80 bg-gray-200 rounded md:rounded-2xl mb-3">
-            </div>
-            <div className="h-4 bg-gray-200 rounded w-2/4 mb-2 ml-1" />
-            <div className="h-1 bg-gray-200 rounded w-2/4 mb-2 ml-1" />
-            <div className="h-8 bg-gray-200 rounded w-4/4 ml-1 mr-1" />
-          </a>
-        </div>
-        <div className="rounded-[12px] text-xl mb-2 p-0 animate-pulse">
-          <a href="/">
-            <div className="w-full h-48 md:h-80 bg-gray-200 rounded md:rounded-2xl mb-3">
-            </div>
-            <div className="h-4 bg-gray-200 rounded w-2/4 mb-2 ml-1" />
-            <div className="h-1 bg-gray-200 rounded w-2/4 mb-2 ml-1" />
-            <div className="h-8 bg-gray-200 rounded w-4/4 ml-1 mr-1" />
-          </a>
-        </div>
-
-      <div className='text-center justify-center mt-6 -mb-16'>
-        <div className="animate-spin inline-block w-10 h-10 border-[3px] border-current border-t-transparent text-gray-400 rounded-full" role="status" aria-label="loading">
+          </div>
+          <div className="h-4 bg-gray-200 rounded w-2/4 mb-2 ml-1" />
+          <div className="h-1 bg-gray-200 rounded w-2/4 mb-2 ml-1" />
+          <div className="h-8 bg-gray-200 rounded w-4/4 ml-1 mr-1" />
+        </a>
+      </div>
+      <div className="rounded-[12px] text-xl mb-2 p-0 animate-pulse">
+        <a href="/">
+          <div className="w-full h-48 md:h-80 bg-gray-200 rounded md:rounded-2xl mb-3">
+          </div>
+          <div className="h-4 bg-gray-200 rounded w-2/4 mb-2 ml-1" />
+          <div className="h-1 bg-gray-200 rounded w-2/4 mb-2 ml-1" />
+          <div className="h-8 bg-gray-200 rounded w-4/4 ml-1 mr-1" />
+        </a>
+      </div>
+      <div className="rounded-[12px] text-xl mb-2 p-0 animate-pulse">
+        <a href="/">
+          <div className="w-full h-48 md:h-80 bg-gray-200 rounded md:rounded-2xl mb-3">
+          </div>
+          <div className="h-4 bg-gray-200 rounded w-2/4 mb-2 ml-1" />
+          <div className="h-1 bg-gray-200 rounded w-2/4 mb-2 ml-1" />
+          <div className="h-8 bg-gray-200 rounded w-4/4 ml-1 mr-1" />
+        </a>
+      </div>
+      <br />
+      <div className=' text-center justify-center mt-2 md:ml-64 md:pl-6 -ml-44'>
+        <div className="animate-spin inline-block w-8 h-8 md:w-10 md:h-10 border-[3px] border-current border-t-transparent text-gray-400 rounded-full" role="status" aria-label="loading">
         </div>
       </div>
 
